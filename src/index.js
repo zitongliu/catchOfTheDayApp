ab540dd28edd628094d60fed2251c7b05d573265
zitongliu <zitongliu7@gmail.com>
2017-04-18 22:26:37 +1000

    store picker wip

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-console.log('yeah!');
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
