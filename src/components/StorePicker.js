1055ea780fa724a3a3fa42b2f67565b3fe002958
zitongliu <zitongliu7@gmail.com>
2017-04-19 22:46:56 +1000

    component wip

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    // Anywhere else
+    return (
+      <form className="store-selector">
+        { /* Comment inside return */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store -></button>
+      </form>
+    )
   }
 }
 
