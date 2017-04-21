5f04966a43b8daf3727995ba5e892725e2e352e1
zitongliu <zitongliu7@gmail.com>
2017-04-21 16:56:54 +1000

    commit local changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>Catch of the Day</h1>
+        <h3 age="5000" cool={true} className="tagline">{this.props.tagline}</h3>
+      </header>
     )
   }
 }
