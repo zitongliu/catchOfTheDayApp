5e6d0e1fc36476828b3e7d4c5be342b816938b3b
zitongliu <zitongliu7@gmail.com>
2017-05-07 14:02:18 +1000

    add fish form - state wip

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,30 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+    // ... is spread. take every item from our object (fishes) and spread it into this object. This takes a copy of our existing state and put it into the fishes state
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    // this.setState({ fishes: fishes});
+    this.setState({ fishes });
+
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +35,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
 
     )
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,7 +7,7 @@ class Inventory extends React.Component {
 
       <div>
         <h2>Inventory</h2>
-        <AddFishForm />
+        <AddFishForm addFish={this.props.addFish}/>
       </div>
 
     )
