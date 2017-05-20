59906545badedb6659cf4b7bb6491d131323c32e
zitongliu <zitongliu7@gmail.com>
2017-05-20 22:30:47 +1000

    Persisting state with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBPa06ctLPI51uKrXWteIO1chpluqolM-A",
+  authDomain: "catch-of-the-day-steve-liu.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-steve-liu.firebaseio.com",
+});
+
+export default base;
