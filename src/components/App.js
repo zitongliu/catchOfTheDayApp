a887961bca6cdd57ae0fc21ef368a8d52a6e9e53
zitongliu <zitongliu7@gmail.com>
2017-06-01 12:22:53 +1000

    Added local storage

--- src/components/App.js
+++ src/components/App.js
@@ -23,15 +23,30 @@ class App extends React.Component {
   }
 
   componentWillMount() {
+    // this runs right before the app is rendered
     this.ref = base.syncState(`${this.props.params.storeId}/fishes`,
       {
         context: this,
         state: 'fishes'
       });
+
+      // check if there is any order in localStorage
+      const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+      if(localStorageRef) {
+        // update our App component's order state
+        this.setState({
+          order: JSON.parse(localStorageRef)
+        });
+      }
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
 
   addFish(fish) {
@@ -76,7 +91,7 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
 
